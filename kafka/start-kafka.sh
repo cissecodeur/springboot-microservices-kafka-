a7e2518e13ea68cf4d7fc164550433861f1eed71
yacoubc01@gmail.com <yacoubc01@gmail.com>
2023-11-12 18:04:40 +0100

    ms with kafka

--- kafka/start-kafka.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/ray/Documents/INTELIJ-PROJECT/MICROSERVICES-CQRS/kafka_2.13-3.5.0/bin && 
-./kafka-server-start.sh /Users/ray/Documents/INTELIJ-PROJECT/MICROSERVICES-CQRS/kafka_2.13-3.5.0/config/server.properties
--- kafka/start-zookeeper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd kafka_2.13-3.5.0/bin && ./zookeeper-server-start.sh ../config/zookeeper.properties
-
